929d18cbcff98a3770f6130eb67f6156f37f641e
ahmed <ahmed.nageeb.mahmoud.hssan@gmail.com>
2023-03-22 20:13:41 +0200

    Serilog Finsh

--- BackEnd/Draw.API/Program.cs
+++ BackEnd/Draw.API/Program.cs
@@ -10,17 +10,58 @@ using Microsoft.IdentityModel.Tokens;
 using Draw.BLL.AuthBLL;
 using Draw.BLL.DiagramBLL;
 using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+try
+{
+    //To Apply appsettings.json configrations you need to ConfigurationBuilder to easy do that
+    var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json") // Add main appsettings.json
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true) // if you have setting file for every environment
+            .Build();
 
-Log.Logger = new LoggerConfiguration()
-    .CreateLogger();
+    //Now Create Logger And Pass Configration And Set File Configation
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(configuration)
+        //I Prefer You Add the Configuration File Here Because It's Available To Add Args In File Path Like Date Or Else
+        //Write As Text
+        .WriteTo.File(
+        //Log Path
+        path: "./logs/log-text-",
+        //Every Day Log In A New File
+        rollingInterval: RollingInterval.Day,
+         //Log Text Formatter
+         outputTemplate: "[{Timestamp:HH:mm:ss} => {SourceContext} => [{Level}] => {Message}{NewLine}{Exception}"
+        )
+      //Write Also As Json
+      .WriteTo.File(
+        //Log Path
+        path: "./logs/log-json-",
+        //Every Day Log In A New File
+        rollingInterval: RollingInterval.Day,
+        //Or Log As Josn Format
+        formatter: new Serilog.Formatting.Json.JsonFormatter(",")
+        )
+        .CreateLogger();
 
-//User Serilog
-builder.Host.UseSerilog();
+    //User Serilog
+    builder.Host.UseSerilog();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "I Can't Run The Project");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
 
 //Add Cors And Create Policy For Ng App
-builder.Services.AddCors(c =>c.AddPolicy("ngApp", options =>options.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddCors(c => c.AddPolicy("ngApp", options => options.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader()));
 
 // Add services to the container.
 
@@ -56,7 +97,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 //Add JWT Service And Init Confgrations
 builder.Services.AddAuthentication(options =>
 {
-    
+
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(o =>
@@ -95,17 +136,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
-{
-
 app.Run();
-    
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "I Can't Run The Project");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
